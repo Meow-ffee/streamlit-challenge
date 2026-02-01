29e829fa81aa871e1d972cc5d87c84b09ec700ac
Meow-ffee <choc.mitsu@gmail.com>
2026-02-01 14:30:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,10 @@ def get_dominant_colors(img, n_colors=3):
 # ----------------------------------------
 # UI
 # ----------------------------------------
-st.title("📸 カメラ・アップロードでカラー判定")
+st.markdown(
+    "<h1 style='white-space: nowrap;'>📸 カメラ・アップロードでカラー判定</h1>",
+    unsafe_allow_html=True
+)
 st.write("画像から代表色を抽出して **色名 / HEX / RGB** を表示します。")
 
 input_type = st.radio(
